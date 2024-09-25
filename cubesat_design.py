8e63db3e6d0349634c1ff8e6ae3022635ea57e6d
Gabriel Alves Silva <gabriel_cte@outlook.com>
2024-09-25 20:40:44 -0300

    Scripts para simulação usando o JSBSIM e o FlightGear Signed-off-by: Gabriel Alves Silva <gabriel_cte@outlook.com>

--- cubesat_design.py
+++ cubesat_design.py
@@ -1,6 +1,46 @@
 # This scrip is for the calculation to set the cubesat configuration.
 # Imports
 import numpy as np
+import math
+# Funções
+mu_Terra = 3.98600441e+014 # [m^3/s^2]
+
+def osv2eko(r_SCGI, v_SCGI):
+    global mu_Terra
+    # versores sistema de coordenadas inercial centrado na terra J2000
+    vec_k = np.array([0, 0, 1])
+    vec_i = np.array([1, 0, 0])
+    # Elementos orbitais clássicos
+    h = np.cross(r_SCGI, v_SCGI) # [m^2/s]
+    p = np.dot(h, h)/mu_Terra # [m]
+    B = np.cross(v_SCGI,h) - mu_Terra * r_SCGI / np.linalg.norm(r_SCGI) # [m^3/s^2]
+    e = B/mu_Terra
+    a = p/(1-np.dot(e, e)) # [m]
+    vec_N = np.cross(vec_k, h) # [m^2/s]
+    i = np.arccos(np.dot(vec_k, h)/np.linalg.norm(h))
+    RAAN = np.arccos(np.dot(vec_i, vec_N)/np.linalg.norm(vec_N)) # [rad]
+    omega = np.arccos(np.dot(e, vec_N)/(np.linalg.norm(e)*np.linalg.norm(vec_N))) # [rad]
+    f = np.real(np.arccos(np.dot(r_SCGI, e)/(np.linalg.norm(e)*np.linalg.norm(r_SCGI)))) # [rad]
+    return  [a, e, i, RAAN, omega, f]
+
+def eko2osv(a, e, i, RAAN, omega, f):
+    global mu_Terra
+    e = np.linalg.norm(e)
+    p = a*(1-e**2)
+    T = np.zeros((3, 3))
+    u = omega+f
+    T[0,0] = np.cos(RAAN)*np.cos(u)-np.sin(RAAN)*np.sin(u)*np.cos(i)
+    T[0,1] = np.sin(RAAN)*np.cos(u)+np.cos(RAAN)*np.sin(u)*np.cos(i)
+    T[0,2] = np.sin(u)*np.sin(i)
+    T[1,0] = -np.cos(RAAN)*np.sin(u)-np.sin(RAAN)*np.cos(u)*np.cos(i)
+    T[1,1] = -np.sin(RAAN)*np.sin(u)+np.cos(RAAN)*np.cos(u)*np.cos(i)
+    T[1,2] = np.cos(u)*np.sin(i)
+    T[2,0] = np.sin(RAAN)*np.sin(i)
+    T[2,1] = -np.cos(RAAN)*np.sin(i)
+    T[2,2] = np.cos(i)
+    r_SCGI = np.matmul(np.transpose(T), np.array([[p/(1+e*np.cos(f))], [0], [0] ]))
+    v_SCGI = np.matmul(math.sqrt(mu_Terra/p)*np.transpose(T),np.array([[e*np.sin(f)], [(1+e*np.cos(f))],[0]]))
+    return [r_SCGI, v_SCGI]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -26,4 +66,23 @@ if __name__ == '__main__':
 
     print('Matrix of Inertia')
     print(I)
+
+    # Determinando Órbita
+    r_SCGI = np.array([2.25526213722520e+006, -3.00492371279401e+006, -5.84397331427593e+006]) # m
+    v_SCGI = np.array([-5.19923341417592e+003, 3.82519438208177e+003, -3.97333292224794e+003]) # m/s
+
+    [a, e, i, RAAN, omega, f] = osv2eko(r_SCGI, v_SCGI)
+    print('Elementos Clássicos de Órbita:')
+    print(f'Semi-eixo maior: {a} [m]')
+    print(f'Excentricidade: {e}')
+    print(f'Inclinação: {i}')
+    print(f'Longitude do nodo ascendente: {RAAN}')
+    print(f'Argumento do perigeo: {omega}')
+    print(f'Anomalia verdadeira: {f}')
+
+    [r_SCGI, v_SCGI]= eko2osv(a, e, i, RAAN, omega, f)
+
+    print('Vetores de estado da órbita:')
+    print(f'distancia: {r_SCGI} [m]')
+    print(f'velocidade: {v_SCGI} [m/s]')
 
