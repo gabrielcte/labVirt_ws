76be032fe4f86e26744da7f9633a460298a22e51
Gabriel Alves Silva <gabriel_cte@outlook.com>
2024-10-07 22:10:23 -0300

    Trajetória da òrbita Signed-off-by: Gabriel Alves Silva <gabriel_cte@outlook.com>

--- cubesat_design.py
+++ cubesat_design.py
@@ -1,12 +1,20 @@
 # This scrip is for the calculation to set the cubesat configuration.
 # Imports
-import numpy as np
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.image as mpimg
 
-
-# Funções
+# Variáveis globais
 mu_Terra = 3.98600441e+014 # [m^3/s^2]
+raioEquatorialTerra = 6.378136e+6 # [m]
+raioPolarTerra = 6.356753e+6 # [m]
 sequencia_rotacao = np.array([3, 2, 1])
+
+def rad2deg(angle_rad):
+    angle_deg = angle_rad/np.pi*180
+    return angle_deg
 
 def eulerAngles2C (theta_1, theta_2, theta_3):
     global sequencia_rotacao
@@ -16,15 +24,15 @@ def eulerAngles2C (theta_1, theta_2, theta_3):
         if sequencia_rotacao[seq] == 1:
             e_x = angles[seq]
             rot_x = np.array([[1, 0, 0], [0, np.cos(e_x), np.sin(e_x)], [0, -np.sin(e_x), np.cos(e_x)]])
-            C = C * rot_x
+            C = np.matmul(C, rot_x)
         elif sequencia_rotacao[seq] == 2:
             e_y = angles[seq]
             rot_y = np.array([[np.cos(e_y), 0, -np.sin(e_y)], [0, 1, 0], [np.sin(e_y), 0, np.cos(e_y)]])
-            C = C*rot_y
+            C = np.matmul(C, rot_y)
         elif sequencia_rotacao[seq] == 3:
             e_z = angles[seq]
             rot_z = np.array([[np.cos(e_z), np.sin(e_z), 0], [-np.sin(e_z), np.cos(e_z), 0], [0, 0, 1]])
-            C = C*rot_z
+            C = np.matmul(C, rot_z)
         else:
             print('Erro em calcular a matriz de rotação')
     return C
@@ -53,56 +61,69 @@ def eko2osv(a, e, i, RAAN, omega, f):
     p = a*(1-e**2)
     T = np.zeros((3, 3))
     u = omega+f
-    T[0,0] = np.cos(RAAN)*np.cos(u)-np.sin(RAAN)*np.sin(u)*np.cos(i)
-    T[0,1] = np.sin(RAAN)*np.cos(u)+np.cos(RAAN)*np.sin(u)*np.cos(i)
-    T[0,2] = np.sin(u)*np.sin(i)
-    T[1,0] = -np.cos(RAAN)*np.sin(u)-np.sin(RAAN)*np.cos(u)*np.cos(i)
-    T[1,1] = -np.sin(RAAN)*np.sin(u)+np.cos(RAAN)*np.cos(u)*np.cos(i)
-    T[1,2] = np.cos(u)*np.sin(i)
-    T[2,0] = np.sin(RAAN)*np.sin(i)
-    T[2,1] = -np.cos(RAAN)*np.sin(i)
-    T[2,2] = np.cos(i)
+    T[0][0] = np.cos(RAAN)*np.cos(u)-np.sin(RAAN)*np.sin(u)*np.cos(i)
+    T[0][1] = np.sin(RAAN)*np.cos(u)+np.cos(RAAN)*np.sin(u)*np.cos(i)
+    T[0][2] = np.sin(u)*np.sin(i)
+    T[1][0] = -np.cos(RAAN)*np.sin(u)-np.sin(RAAN)*np.cos(u)*np.cos(i)
+    T[1][1] = -np.sin(RAAN)*np.sin(u)+np.cos(RAAN)*np.cos(u)*np.cos(i)
+    T[1][2] = np.cos(u)*np.sin(i)
+    T[2][0] = np.sin(RAAN)*np.sin(i)
+    T[2][1] = -np.cos(RAAN)*np.sin(i)
+    T[2][2] = np.cos(i)
     r_SCGI = np.matmul(np.transpose(T), np.array([[p/(1+e*np.cos(f))], [0], [0] ]))
     v_SCGI = np.matmul(math.sqrt(mu_Terra/p)*np.transpose(T),np.array([[e*np.sin(f)], [(1+e*np.cos(f))],[0]]))
     return [r_SCGI, v_SCGI]
 
 
 
-# def posicaoVeiculoEspacialOrbitaCircularReferenciaInercial(e, a, p, i, RAAN, omega, f, num_Orbitas, dt):
-#     global sequencia_rotacao
-#     x_scgi = np.zeros(num_Orbitas, 1)
-#     x_scgi = np.zeros(num_Orbitas, 1)
-#     x_scgi = np.zeros(num_Orbitas, 1)
-#     lat = np.zeros(num_Orbitas, 1)
-#     lon = np.zeros(num_Orbitas, 1)
-#     C = angulosEulerParaC (-RAAN, -i, -omega);
-#     for contador = 1:num_Orbitas
-#         E = omega+f;
-#         x_scgi(contador) = (p/(1+norm(e)*cos(f)))*(cos(norm(RAAN))*cos(E)-sin(norm(RAAN))*sin(E)*cos(norm(i)));
-#         x_scgi(contador) = (p/(1+norm(e)*cos(f)))*(sin(norm(RAAN))*cos(E)+cos(norm(RAAN))*sin(E)*cos(norm(i)));
-#         x_scgi(contador) = (p/(1+norm(e)*cos(f)))*(sin(E)*sin(norm(i)));
-#         f = f+dt;
-#     # Trajetória Polar
-#       auxiliar(contador) =(f);
-#       raioCoordenadaPolar(contador) = a*(1-dot(e,e))/(1+norm(e)*cos(auxiliar(contador)));
-#       e_xpolar(contador) = raioCoordenadaPolar(contador)*cos(f);
-#       e_ypolar(contador) = raioCoordenadaPolar(contador)*sin(f);
-#       e_zpolar(contador) = 0;
-#       f = f+dt;
-#     # Trajetória SGI
-#         velocidadePolar(1,1) = e_xpolar(contador);
-#         velocidadePolar(2,1) = e_ypolar(contador);
-#         velocidadePolar(3,1) = e_zpolar(contador);
-#         velocidadeSistemaGeocentricoInercial = C*velocidadePolar;
-#         e_xSistemaGeocentricoInercial(contador) = velocidadeSistemaGeocentricoInercial(1,1);
-#         e_ySistemaGeocentricoInercial(contador) = velocidadeSistemaGeocentricoInercial(2,1);
-#         e_zSistemaGeocentricoInercial(contador) = velocidadeSistemaGeocentricoInercial(3,1);
-#     # Ground Track
-#       lat(contador) = rad2deg(atan2(e_zSistemaGeocentricoInercial(contador),sqrt(e_xSistemaGeocentricoInercial(contador)^2+e_ySistemaGeocentricoInercial(contador)^2)));
-#       lon(contador) = rad2deg(atan2(e_ySistemaGeocentricoInercial(contador),e_xSistemaGeocentricoInercial(contador)));
-#       if lon(contador) < 0;
-#         lon(contador) = lon(contador);
-#     return [x_scgi, x_scgi, x_scgi, lat, lon]
+def trajetoria(e, a, i, RAAN, omega, f, num_Orbitas, dt):
+    x_scgi = np.zeros((num_Orbitas, 1))
+    y_scgi = np.zeros((num_Orbitas, 1))
+    z_scgi = np.zeros((num_Orbitas, 1))
+    lat = np.zeros((num_Orbitas, 1))
+    lon = np.zeros((num_Orbitas, 1))
+    aux = np.zeros((num_Orbitas, 1))
+    r_polar = np.zeros((num_Orbitas, 1))
+    e_x_scgi = np.zeros((num_Orbitas, 1))
+    e_y_scgi = np.zeros((num_Orbitas, 1))
+    e_z_scgi = np.zeros((num_Orbitas, 1))
+    e_polar = np.zeros((3, 1))
+    e_xpolar = np.zeros((num_Orbitas, 1))
+    e_ypolar = np.zeros((num_Orbitas, 1))
+    e_zpolar = np.zeros((num_Orbitas, 1))
+    p = a * (1 - e ** 2)
+    p = np.linalg.norm(p)
+    C = eulerAngles2C (-RAAN, -i, -omega)
+    for stps in range(num_Orbitas):
+        E = omega+f
+        x_scgi[stps] = (p/(1+np.linalg.norm(e)*np.cos(f)))*(np.cos(RAAN)*np.cos(E)-np.sin(RAAN)*np.sin(E)*np.cos(i))
+        y_scgi[stps] = (p/(1+np.linalg.norm(e)*np.cos(f)))*(np.sin(RAAN)*np.cos(E)+np.cos(RAAN)*np.sin(E)*np.cos(i))
+        z_scgi[stps] = (p/(1+np.linalg.norm(e)*np.cos(f)))*(np.sin(E)*np.sin(i))
+
+        # Trajetória Polar
+        aux[stps]= f
+        r_polar[stps] = a*(1-np.dot(e,e))/(1+np.linalg.norm(e)*np.cos(aux[stps]))
+        e_xpolar[stps] = r_polar[stps] * np.cos(f)
+        e_ypolar[stps] = r_polar[stps] * np.sin(f)
+        e_zpolar[stps] = 0
+        f += dt
+
+        # Trajetória SGI
+
+        e_polar[0][0] = e_xpolar[stps]
+        e_polar[1][0] = e_ypolar[stps]
+        e_polar[2][0] = e_zpolar[stps]
+        e_scgi = np.matmul(C, e_polar)
+        e_x_scgi[stps] = e_scgi[0][0]
+        e_y_scgi[stps] = e_scgi[1][0]
+        e_z_scgi[stps] = e_scgi[2][0] ##
+
+        # Ground Track
+        lat[stps]= rad2deg(np.arctan2(e_z_scgi[stps],np.sqrt(e_x_scgi[stps]**2+e_y_scgi[stps]**2)))
+        lon[stps]= rad2deg(np.arctan2(e_y_scgi[stps],e_x_scgi[stps]))
+        if lon[stps]< 0:
+            lon[stps] = lon[stps]
+    return [x_scgi, y_scgi, z_scgi, lat, lon]
 
 
 # Press the green button in the gutter to run the script.
@@ -149,3 +170,88 @@ if __name__ == '__main__':
     print(f'distancia no SCGI: {r_SCGI} [m]')
     print(f'velocidade no SCGI: {v_SCGI} [m/s]')
 
+    [x_scgi, y_scgi, z_scgi, lat, lon]= trajetoria(e=e, a=a, i=i, RAAN=RAAN, omega=omega, f=f, num_Orbitas=720, dt=1)
+
+    plt.figure(1)
+    plt.title('Ground Track')
+    plt.plot(lon, lat,'.b')
+
+    # Ler a imagem
+
+    contour = mpimg.imread('mapaContorno.jpg')
+
+    # Configurar a exibição da imagem
+    plt.imshow(contour, extent=[-180, 180, -90, 90])
+
+    # Configurar os rótulos dos eixos
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+
+    # Definir limites dos eixos
+    plt.xlim([-180, 180])
+    plt.ylim([-90, 90])
+
+    # Definir as marcações dos eixos x
+    plt.xticks([-180, -150, -120, -90, -60, -30, 0, 30, 60, 90, 120, 150, 180],
+               ['180W', '150W', '120W', '90W', '60W', '30W', '0', '30E', '60E', '90E', '120E', '150E', '180E'])
+
+    # Definir as marcações dos eixos y
+    plt.yticks([-90, -75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75, 90],
+               ['90N', '75N', '60N', '45N', '30N', '15N', '0', '15S', '30S', '45S', '60S', '75S', '90S'])
+
+    # Configurar a grade e proporções iguais
+    plt.axis('on')
+    plt.axis('equal')
+    plt.grid(True)
+
+
+    # syntax for 3-D projection
+    fig = plt.figure(2)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_title('3D Orbit')
+
+    # Carregar a imagem do mapa de topografia
+    topomapa = mpimg.imread('topomapa.jpg')
+
+    # Definir os parâmetros da Terra
+
+    divisoes = 100
+
+    # Criar o ellipsoide
+    phi = np.linspace(0, 2 * np.pi, divisoes)
+    theta = np.linspace(0, np.pi, divisoes)
+    phi, theta = np.meshgrid(phi, theta)
+
+    x = raioEquatorialTerra * np.sin(theta) * np.cos(phi)
+    y = raioEquatorialTerra * np.sin(theta) * np.sin(phi)
+    z = raioPolarTerra * np.cos(theta)
+
+    # Configurar a figura e o eixo 3D
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Mapear a imagem no ellipsoide
+    ax.plot_surface(x, y, z, facecolors=plt.cm.gray(topomapa), edgecolor='none', linewidth=0.1, linestyle=':')
+    # defining all 3 axes
+    z = z_scgi
+    x = x_scgi
+    y = y_scgi
+
+    # Adding labels to the axes
+
+    # Configurar os eixos
+    ax.set_xlim([-raioEquatorialTerra, raioEquatorialTerra])
+    ax.set_ylim([-raioEquatorialTerra, raioEquatorialTerra])
+    ax.set_zlim([-raioPolarTerra, raioPolarTerra])
+
+    # plotting
+    ax.plot3D(x, y, z, '.b')
+
+    # Configurar a visualização
+    ax.view_init(30, -30)
+    plt.axis('on')
+    plt.axis('equal')
+    plt.grid(True)
+
+
+# Mostrar a imagem
+    plt.show()
