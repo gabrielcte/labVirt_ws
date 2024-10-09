f1dd3513a2e32596394c8815cee39ddca5576525
Gabriel Alves Silva <gabriel_cte@outlook.com>
2024-10-08 21:01:20 -0300

    Configuração do script da simulação da dinamica do Cubesat Signed-off-by: Gabriel Alves Silva <gabriel_cte@outlook.com>

--- dynamic_simulation.py
+++ dynamic_simulation.py
@@ -1,7 +1,4 @@
-import sys
-
 from pathlib import Path
-from enum import Enum
 import csv
 import jsbsim_utils as jsbsu
 import cubesat_plots as plots
@@ -9,30 +6,6 @@ import jsbsim
 import navpy
 import numpy as np
 import time
-
-
-
-class FlightStages(Enum):
-    flight_stage_cruise = 0
-    flight_stage_turning = 1
-    flight_stage_falling = 2
-    flight_stage_trajectory_hold = 3
-
-
-def ramp_function(x0, xf, t0, delta_t, tk):
-    tf = t0 + delta_t
-
-    # Verifica se o tempo atual excede o tempo total da rampa
-    if tk > t0 + delta_t:
-        return xf  # Retorna o valor final se o tempo exceder a rampa
-    elif tk < t0:
-        return x0
-
-    # Calcula o valor da rampa linearmente com base no tempo atual
-    xk = x0 + (xf - x0) * (tk - t0) / delta_t
-
-    return xk
-
 
 root = (Path('.')).resolve()  # Define o caminho absoluto para o diretório "aircrafts"
 fdm = jsbsim.FGFDMExec(
@@ -47,28 +20,13 @@ fdm.load_model(aircraft)
 filename_con = 'fg_conn.xml'  # Arquivo de conexão com o FlightGear
 filename_log = 'simulation_data.csv'  # Arquivo de conexão com o FlightGear
 realtime = False
-sim_dt = 0.004
-sim_period = 10000
+sim_dt = 0.05
+sim_period = 720
 
-# Localização inicial: Aeroporto do Qatar, Umm Garn airport.
-h0 = 34.000366848  # Elevação aproximada do terreno (m)
-lat0_deg = 25.7532  # Latitude (graus)
-lon0_deg = 51.2926  # Longitude (graus)
 
-# Rampa e Missão
-ramp_init_val = 0.00
-ramp_final_val = np.deg2rad(-80)
-ramp_time = 12.00
-max_elevator_angle = np.deg2rad(18)
-pitch_final_val = np.deg2rad(-70)
-target_pitch = ramp_init_val
-
-# PID
-kp_alfa = -0.8
-ki_alfa = -0.04
-kd_alfa = -1
-kd_filter_fc_alfa = 80.0
-
+h0 = 569366.9993  # Elevação aproximada do terreno (m)
+lat0_deg = -82.365  # Latitude (graus)
+lon0_deg = -41.076  # Longitude (graus)
 
 
 output_path = Path(
@@ -84,22 +42,19 @@ fdm.set_dt(sim_dt)  # Define o passo da simulação (s)
 jsbsu.set_location(fdm, lat0_deg, lon0_deg, h0)  # Aplicar a localização inicial
 jsbsu.set_ic0(fdm)
 
-fdm['ic/h-sl-ft'] = 400000  # Define a altitude inicial (pés)
-fdm['ic/phi-rad'] = 0
-fdm['ic/psi-true-rad'] = 0
-fdm['ic/alpha-rad'] = 0
-fdm['ic/beta-rad'] = 0
-fdm['ic/gamma-deg'] = 0
+fdm['ic/h-sl-ft'] = h0  # Define a altitude inicial (pés)
+fdm['ic/phi-rad'] = np.deg2rad(30)
+fdm['ic/theta-rad'] = np.deg2rad(45)
+fdm['ic/psi-true-rad'] = np.deg2rad(60)
 
 # Liga os motores
 prop = fdm.get_propulsion()
 prop.init_running(0)
 
-# Superfícies de controle
-
-
 # Potência dos motores
-fdm['fcs/throttle-cmd-norm[0]'] = 1
+fdm['fcs/tvc_inertial_x'] = 0
+fdm['fcs/tvc_inertial_y'] = 0
+fdm['fcs/tvc_inertial_x'] = 0
 
 # Abre o arquivo CSV para escrita
 csvfile = open(filename_log, 'w', newline='')
@@ -108,12 +63,7 @@ writer.writerow(['Time (s)',
                  'Latitude (rad)', 'Longitude (rad)', 'Altitude (m)', 'Height (m)',
                  'Pos N (m)', 'Pos E (m)', 'Pos D (m)',
                  'Vel X (m/s)', 'Vel Y (m/s)', 'Vel Z (m/s)', 'Vel Module (m/s)',
-                 'Roll (rad)', 'Pitch (rad)', 'Yaw (rad)', 'Target Pitch (rad)',
-                 'Angle of Attack (rad)', 'Angle of Sideslip (rad)',
-                 'Elevator CMD (rad)', 'Elevator Actuator (rad)', 'Elevator Control (deg)',
-                 'Rudder CMD (rad)', 'Rudder Actuator (rad)', 'Rudder Control (deg)',
-                 'Aileron CMD (rad)', 'Aileron Actuator (rad)', 'Aileron Control (deg)',
-                 'Throttle CMD'])
+                 'Roll (rad)', 'Pitch (rad)', 'Yaw (rad)'])
 
 # Variáveis do loop de simulação
 dt = fdm.get_delta_t()
@@ -123,14 +73,7 @@ sim_time = fdm.get_sim_time()
 frame_time = 0
 frame_period = 0.03
 
-# pid_alfa = dsp.PIDController(kp_alfa, ki_alfa, kd_alfa, kd_filter_fc_alfa, dt)
 
-flight_stage_current = FlightStages.flight_stage_cruise
-
-k_q = 0.656
-k_theta = 1.7
-cruise_duration = 60
-turn_duration = 60
 
 try:
 
@@ -144,84 +87,83 @@ try:
         lla_deg = [np.rad2deg(sim_data['lat']), np.rad2deg(sim_data['lon']), sim_data['alt']]
         xyz = navpy.lla2ned(lla_deg[0], lla_deg[1], lla_deg[2], lat0_deg, lon0_deg, h0)
         vel_module = np.sqrt(sim_data['vel_x'] ** 2 + sim_data['vel_y'] ** 2 + sim_data['vel_z'] ** 2)
-
-        if flight_stage_current == FlightStages.flight_stage_cruise:
-
-            op_cruise['ic/theta-rad'] = op_cruise['ic/gamma-rad'] + op_cruise['ic/alpha-rad']
-            elevator_cmd = op_cruise['fcs/elevator-cmd-norm']
-            elevator_cmd += k_q * fdm['velocities/q-rad_sec']
-            elevator_cmd += k_theta * (fdm['attitude/theta-rad'] - op_cruise['ic/theta-rad'])
-            elevator_cmd *= max_elevator_angle
-            flight_stage_current = FlightStages.flight_stage_turning
-            if sim_data['sim_time'] > cruise_duration:
-                print('### TIME REACHED!!')
-                # break
-
-        if flight_stage_current == FlightStages.flight_stage_turning:
-
-            aileron_cmd = op_turn['fcs/elevator-cmd-norm']
-            aileron_cmd *= max_elevator_angle
-
-            elevator_cmd = op_turn['fcs/elevator-cmd-norm']
-            elevator_cmd *= max_elevator_angle
-
-            rudder_cmd = op_turn['fcs/elevator-cmd-norm']
-            rudder_cmd *= max_elevator_angle
-
-            if sim_data['sim_time'] > turn_duration:
-                flight_stage_current = FlightStages.flight_stage_falling
-                print('### TIME REACHED!!')
-                # break
-
-        elif flight_stage_current == FlightStages.flight_stage_falling:
-
-            target_pitch = ramp_function(ramp_init_val, ramp_final_val, cruise_duration, ramp_time,
-                                         sim_data['sim_time'])
-
-            elevator_cmd = op_terminal_dive['fcs/elevator-cmd-norm']
-            elevator_cmd += k_q * (fdm['velocities/q-rad_sec'] - op_terminal_dive['ic/q-rad_sec'])
-            elevator_cmd += k_theta * (fdm['attitude/theta-rad'] - target_pitch)
-            elevator_cmd *= max_elevator_angle
-
-            if sim_data['pitch'] < pitch_final_val:
-                flight_stage_current = FlightStages.flight_stage_trajectory_hold
-                print('### ANGLE REACHED!!')
-                # break
-
-        elif flight_stage_current == FlightStages.flight_stage_trajectory_hold:
-
-            op_terminal_lock['ic/theta-rad'] = op_terminal_lock['ic/gamma-rad'] + op_terminal_lock['ic/alpha-rad']
-            elevator_cmd = op_terminal_lock['fcs/elevator-cmd-norm']
-            elevator_cmd += k_q * fdm['velocities/q-rad_sec']
-            elevator_cmd += k_theta * (fdm['attitude/theta-rad'] - op_terminal_lock['ic/theta-rad'])
-            elevator_cmd *= max_elevator_angle
-
-        else:
-            raise Exception('### ERROR: undefined flight stage!')
-
-        fdm['fcs/elevator-cmd-norm'] = (elevator_cmd / max_elevator_angle)
-        fdm['fcs/rudder-cmd-norm'] = (rudder_cmd / max_elevator_angle)
-        fdm['fcs/aileron-cmd-norm'] = (aileron_cmd / max_elevator_angle)
+        #
+        # if flight_stage_current == FlightStages.flight_stage_cruise:
+        #
+        #     op_cruise['ic/theta-rad'] = op_cruise['ic/gamma-rad'] + op_cruise['ic/alpha-rad']
+        #     elevator_cmd = op_cruise['fcs/elevator-cmd-norm']
+        #     elevator_cmd += k_q * fdm['velocities/q-rad_sec']
+        #     elevator_cmd += k_theta * (fdm['attitude/theta-rad'] - op_cruise['ic/theta-rad'])
+        #     elevator_cmd *= max_elevator_angle
+        #     flight_stage_current = FlightStages.flight_stage_turning
+        #     if sim_data['sim_time'] > cruise_duration:
+        #         print('### TIME REACHED!!')
+        #         # break
+        #
+        # if flight_stage_current == FlightStages.flight_stage_turning:
+        #
+        #     aileron_cmd = op_turn['fcs/elevator-cmd-norm']
+        #     aileron_cmd *= max_elevator_angle
+        #
+        #     elevator_cmd = op_turn['fcs/elevator-cmd-norm']
+        #     elevator_cmd *= max_elevator_angle
+        #
+        #     rudder_cmd = op_turn['fcs/elevator-cmd-norm']
+        #     rudder_cmd *= max_elevator_angle
+        #
+        #     if sim_data['sim_time'] > turn_duration:
+        #         flight_stage_current = FlightStages.flight_stage_falling
+        #         print('### TIME REACHED!!')
+        #         # break
+        #
+        # elif flight_stage_current == FlightStages.flight_stage_falling:
+        #
+        #     target_pitch = ramp_function(ramp_init_val, ramp_final_val, cruise_duration, ramp_time,
+        #                                  sim_data['sim_time'])
+        #
+        #     elevator_cmd = op_terminal_dive['fcs/elevator-cmd-norm']
+        #     elevator_cmd += k_q * (fdm['velocities/q-rad_sec'] - op_terminal_dive['ic/q-rad_sec'])
+        #     elevator_cmd += k_theta * (fdm['attitude/theta-rad'] - target_pitch)
+        #     elevator_cmd *= max_elevator_angle
+        #
+        #     if sim_data['pitch'] < pitch_final_val:
+        #         flight_stage_current = FlightStages.flight_stage_trajectory_hold
+        #         print('### ANGLE REACHED!!')
+        #         # break
+        #
+        # elif flight_stage_current == FlightStages.flight_stage_trajectory_hold:
+        #
+        #     op_terminal_lock['ic/theta-rad'] = op_terminal_lock['ic/gamma-rad'] + op_terminal_lock['ic/alpha-rad']
+        #     elevator_cmd = op_terminal_lock['fcs/elevator-cmd-norm']
+        #     elevator_cmd += k_q * fdm['velocities/q-rad_sec']
+        #     elevator_cmd += k_theta * (fdm['attitude/theta-rad'] - op_terminal_lock['ic/theta-rad'])
+        #     elevator_cmd *= max_elevator_angle
+        #
+        # else:
+        #     raise Exception('### ERROR: undefined flight stage!')
+        #
+        # fdm['fcs/elevator-cmd-norm'] = (elevator_cmd / max_elevator_angle)
+        # fdm['fcs/rudder-cmd-norm'] = (rudder_cmd / max_elevator_angle)
+        # fdm['fcs/aileron-cmd-norm'] = (aileron_cmd / max_elevator_angle)
 
         writer.writerow([sim_data['sim_time'],
                          sim_data['lat'], sim_data['lon'], sim_data['alt'], sim_data['height'],
                          xyz[0], xyz[1], xyz[2],
                          sim_data['vel_x'], sim_data['vel_y'], sim_data['vel_z'], vel_module,
-                         sim_data['roll'], sim_data['pitch'], sim_data['yaw'], target_pitch,
-                         sim_data['alpha'], sim_data['beta'],
-                         elevator_cmd, sim_data['ele_act'], sim_data['ele_ctrl'],
-                         rudder_cmd, sim_data['rud_act'], sim_data['rud_ctrl'],
-                         aileron_cmd, sim_data['ail_act'], sim_data['ail_ctrl'],
-                         fdm['fcs/throttle-cmd-norm[0]']])
+                         sim_data['roll'], sim_data['pitch'], sim_data['yaw']])
 
         result = fdm.run()
 
         print(f"Time: {sim_data['sim_time']:.2f} s\
-                   H: {sim_data['height']:.2f} m\
-               Vel X: {sim_data['vel_x']:.2f} m/s\
-             Atuador: {np.rad2deg(elevator_cmd):.2f} deg\
-               Alpha: {np.rad2deg(sim_data['alpha']):.2f} deg\
-               Pitch: {np.rad2deg(sim_data['pitch']):.2f} deg", end='\r', flush=True)
+                Latitude: {sim_data['lat']:.2f} m\
+                Longitude: {sim_data['lon']:.2f} m\
+                H: {sim_data['height']:.2f} m\
+                Vel X: {sim_data['vel_x']:.2f} m/s\
+                Vel Y: {sim_data['vel_Y']:.2f} m/s\
+                Vel Z: {sim_data['vel_Z']:.2f} m/s\
+                Roll: {np.rad2deg(sim_data['phi']):.2f} deg\
+                Pitch: {np.rad2deg(sim_data['phi']):.2f} deg\
+                Yaw: {np.rad2deg(sim_data['yaw']):.2f} deg", end='\r', flush=True)
 
         if sim_data['height'] < 0.1:
             break
